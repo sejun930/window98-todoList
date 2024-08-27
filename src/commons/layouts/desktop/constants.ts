f91f590e10241af89bf5474d8b24eda18c3748c8
sejun930 <sejun3278@naver.com>
2024-08-27 18:54:34 +0900

    [Setting] 레이아웃 - 좌측 영역 설계 및 아이콘 추가 완료

---
+++ src/commons/layouts/desktop/constants.ts
@@ -0,0 +1,24 @@
+interface IDESKTOP_LISTS {
+  name: string; // 파일명
+  src: string; // 파일 이미지
+  href?: string; // 파일 클릭시 오픈될 페이지
+  isBlank?: boolean; // 새창으로 열기 여부
+}
+
+// 화면에 노출될 아이콘 및 이름 정보 리스트
+export const DESKTOP_LISTS: IDESKTOP_LISTS[] = [
+  { name: "휴지통", src: "recycle" },
+  {
+    name: "Internet",
+    src: "internet",
+    href: "https://mcm-js.site/",
+    isBlank: true,
+  },
+  {
+    name: "Github",
+    src: "github",
+    href: "https://github.com/sejun930/front-assignment",
+    isBlank: true,
+  },
+  { name: "Todo-List", src: "documents" },
+];
--- src/commons/layouts/desktop/index.tsx
+++ src/commons/layouts/desktop/index.tsx
@@ -1,4 +1,45 @@
+import styles from "./styles.module.css";
+import { DESKTOP_LISTS } from "./constants";
+import type { IWithLinkProps } from "./types";
+
+import Image from "next/image";
+import Link from "next/link";
+
 // 레이아웃의 왼쪽 영역 아이콘들
 export default function LayoutsDesktop() {
-  return <div>왼쪽 영역입니다</div>;
+  // 이동 경로가 있을 경우, Link 태그와 함께 사용
+  const withLink = ({ children, href, isBlank }: IWithLinkProps) => {
+    if (href)
+      return (
+        <Link href={href} target={isBlank ? "_blank" : "_self"}>
+          {children}
+        </Link>
+      );
+
+    return children;
+  };
+
+  return (
+    <article className={styles.wrapper}>
+      {DESKTOP_LISTS?.map(({ name, src, href, isBlank }) => {
+        return (
+          <div key={`desktop-${name}-${src}`} className={styles.app}>
+            {withLink({
+              children: (
+                <div className={styles.item}>
+                  <div className={styles.image}>
+                    <Image src={`/icons/${src}.png`} alt={name} layout="fill" />
+                  </div>
+
+                  <p>{name}</p>
+                </div>
+              ),
+              href,
+              isBlank,
+            })}
+          </div>
+        );
+      })}
+    </article>
+  );
 }
