44195f4a7174e76f97612bc09732417a649d6153
sejun930 <sejun3278@naver.com>
2024-08-27 18:10:32 +0900

    [Setting] 레이아웃 - 하단 영역 및 UI 추가 완료

--- src/commons/layouts/startbar/index.tsx
+++ src/commons/layouts/startbar/index.tsx
@@ -1,4 +1,26 @@
+// import Link from "next/link";
+import Link from "next/link";
+import styles from "./styles.module.css";
+import Image from "next/image";
+
 // 레이아웃의 하단 영역
 export default function StartBar() {
-  return <footer>하단 영역입니다</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.start}>
+        <Image src="/icons/window.webp" alt="start" width={0} height={0} />
+        <b>시작</b>
+      </div>
+
+      <div className={styles.items}>
+        <div></div>
+        <div className={styles.made}>
+          Made By {""}
+          <Link href="https://github.com/sejun930" target="_blank">
+            sejun930
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
